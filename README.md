e35e818e3a1482aa9774ed7172f52c2536b16dbd
liaoqn <liaoqn@gmail.com>
2017-12-07 15:00:11 +0800

    补充说明文档

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-"# dubbo-direct-consumer" 
+# dubbo-direct-consumer
+客户端直连 Dubbo 服务，测试使用，或者用来验证线上服务。
+
+使用方式
+=====
+具体步骤如下：
+
+1、继承基类 BaseDubboDirectConsumer<T>
+
+	public class DemoServiceDirectConsumer extends BaseDubboDirectConsumer<DemoService> {
+
+	}
+
+2、实现 getUrl() 方法。该方法需要返回 Dubbo 服务的实际地址，注意不是注册中心（Zookeeper）地址
+
+	@Override
+	protected String getUrl() {
+		// dubbo服务地址
+		return "dubbo://127.0.0.1:20880/com.alibaba.dubbo.demo.DemoService";
+	}
+
+3、通过父类中的 service 变量得到需要调用的 Dubbo 服务，通过 service 变量直接调用服务方法
+
+	public void sayHelloTest() {
+		// 直接通过父类中的 service 变量得到 DemoService 服务
+		String hello = service.sayHello("world"); // call remote method
+	}
+
+具体可以参考[https://github.com/liaoqn/dubbo-direct-consumer/blob/master/src/test/java/com/github/dubbo/direct/DemoServiceDirectConsumer.java](https://github.com/liaoqn/dubbo-direct-consumer/blob/master/src/test/java/com/github/dubbo/direct/DemoServiceDirectConsumer.java "DemoServiceDirectConsumer.java")
+
+
--- src/main/java/com/github/dubbo/direct/BaseDubboDirectConsumer.java
+++ src/main/java/com/github/dubbo/direct/BaseDubboDirectConsumer.java
@@ -9,6 +9,7 @@ import com.alibaba.dubbo.config.ReferenceConfig;
  * 
  * 客户端直连dubbo服务<br/>
  * 测试使用，或者方便验证线上服务
+ * 
  * @author liaoqn
  *
  */
@@ -19,47 +20,51 @@ public abstract class BaseDubboDirectConsumer<T> {
 		application = new ApplicationConfig();
 		application.setName("DubboConsumerTest");
 	}
-	
-	protected T service = service();	
+
+	protected T service = service();
 
 	/**
 	 * 返回直连dubbo服务
+	 * 
 	 * @param clazz
 	 * @return
 	 */
 	protected T service(Class<T> clazz) {
 		// 引用远程服务
 		// 此实例很重，封装了与注册中心的连接以及与提供者的连接，请自行缓存，否则可能造成内存和连接泄漏
-		ReferenceConfig<T> reference = new ReferenceConfig<T>(); 
+		ReferenceConfig<T> reference = new ReferenceConfig<T>();
 		reference.setApplication(application);
-		String url = getUrl();	// 直连dubbo服务的URL
+		String url = getUrl(); // 直连dubbo服务的URL
 		// 如果点对点直连，可以用reference.setUrl()指定目标地址，设置url后将绕过注册中心，
 		// 其中，协议对应provider.setProtocol()的值，端口对应provider.setPort()的值，
 		// 路径对应service.setPath()的值，如果未设置path，缺省path为接口名
 		reference.setUrl(url);
-		
+
 		reference.setInterface(clazz);
-		
+
 		return reference.get();
 	}
-	
+
 	/**
 	 * 返回直连dubbo服务
+	 * 
 	 * @return
 	 */
 	protected T service() {
 		// 变相获取 T.class
-		Class <T> entityClass = (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]; 
-		
+		Class<T> entityClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
+
 		return service(entityClass);
 	}
-	
+
 	/**
 	 * 获取直连dubbo服务的URL, dubbo://10.20.130.230:20880/com.xxx.XxxService
+	 * 
 	 * @return
 	 */
 	protected abstract String getUrl();
-	
+
 	public T getService() {
 		return service;
 	}
